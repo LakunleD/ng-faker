b8603fe7205f3c45e3aac609b4ca2c0ad6124a00
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2025-03-02 15:02:31 +0100

    Generating fake bvn data

--- tests/account.spec.js
+++ tests/account.spec.js
@@ -18,4 +18,17 @@ describe('Account: ', function() {
       expect(accountNumber.length).toEqual(10);
     });
   });
+
+  describe('BVN Generation', function() {
+    test('it should return an 11-digit BVN', function() {
+      var bvn = ngfaker.account.bvn();
+      expect(bvn.length).toEqual(11);
+      expect(bvn).toMatch(/^\d{11}$/);
+    });
+
+    test('it should not start with 0', function() {
+      var bvn = ngfaker.account.bvn();
+      expect(bvn[0]).not.toEqual('0');
+    });
+  });
 });
